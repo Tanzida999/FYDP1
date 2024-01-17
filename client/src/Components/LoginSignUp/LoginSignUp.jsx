15c98dbb73b341a79c024a3e63829f71238e2b95
Tanzida Nowshin <nowshinsara555@gmail.com>
2024-01-17 23:07:56 +0600

    Sign Up page created

--- client/src/Components/LoginSignUp/LoginSignUp.jsx
+++ client/src/Components/LoginSignUp/LoginSignUp.jsx
@@ -10,7 +10,7 @@ const LoginSignUp = () =>{
         <div className="header">
             <div className="text">Sign Up</div>
             <div className="underline"></div>
-        </div>
+        </div>  
         <div className="inputs">   
          <div className="input">
             <img src={user_icon} alt="" />
@@ -25,7 +25,11 @@ const LoginSignUp = () =>{
             <input type="password" />
             </div>
             </div>
-
+            <div className="forgot-password">Forgot Password? <span>Click here</span></div>
+        <div className="submit-container">
+            <div className="submit">Sign Up</div>
+            <div className="submit">Login</div>
+        </div>
     </div>
     )
 }
